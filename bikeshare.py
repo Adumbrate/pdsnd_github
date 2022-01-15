2c4641de3e1cd4a1273d2c90e2e8fc591554d179
Titi <animasauntitiloluwa@gmail.com>
2022-01-15 17:48:30 +0100

    refactor: Add view_data function

--- bikeshare.py
+++ bikeshare.py
@@ -186,6 +186,9 @@ def user_stats(df, city):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print("-" * 40)
+
+
+def view_data(df):
     x = 1
     while True:
         raw_data = input("\nLike to see five lines of raw data? Enter 'YES' or 'NO'.\n")
@@ -205,6 +208,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df, city)
+        view_data(df)
 
         restart = input("\nWould you like to restart? Enter yes or no.\n")
         if restart.lower() != "yes":
